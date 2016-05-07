ee52ebec6af5e63d5ace5ec0f4002e2257bfe1ad
vagrant <vagrant@local.drupalgovcon.org>
2016-05-07 20:57:36 +0000

    Automated commit by Travis CI.

--- docroot/sites/all/themes/custom/govcon_2016/templates/html.tpl.php
+++ docroot/sites/all/themes/custom/govcon_2016/templates/html.tpl.php
@@ -42,16 +42,14 @@
  *
  * @ingroup themeable
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+?><!DOCTYPE html>
+<html xml:lang="<?php print $language->language; ?>"  dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-
 
    <link href='https://fonts.googleapis.com/css?family=Playfair+Display+SC:900,900italic,700,700italic|Alegreya+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 
