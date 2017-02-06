c7e8a541d53af80a13e8259ae60cf1235bdacbb5
Travis-CI <noreply@travis-ci.org>
2017-02-06 04:27:39 +0000

    Automated commit by Travis CI for Build 198725158

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit11443022d7cba110908aa2b3599da113::getLoader();
+return ComposerAutoloaderInit1d1eea39bd2521eb3fc7aa6a4f6fed10::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit11443022d7cba110908aa2b3599da113
+class ComposerStaticInit1d1eea39bd2521eb3fc7aa6a4f6fed10
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
@@ -6520,10 +6520,10 @@ class ComposerStaticInit11443022d7cba110908aa2b3599da113
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit11443022d7cba110908aa2b3599da113::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit11443022d7cba110908aa2b3599da113::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit11443022d7cba110908aa2b3599da113::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit11443022d7cba110908aa2b3599da113::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit1d1eea39bd2521eb3fc7aa6a4f6fed10::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit1d1eea39bd2521eb3fc7aa6a4f6fed10::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit1d1eea39bd2521eb3fc7aa6a4f6fed10::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit1d1eea39bd2521eb3fc7aa6a4f6fed10::$classMap;
 
         }, null, ClassLoader::class);
     }
