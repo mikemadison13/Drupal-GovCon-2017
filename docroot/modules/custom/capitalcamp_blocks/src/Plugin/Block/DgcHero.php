4d6d45407e9fcccbbf6676bc69a26075aeb73246
pipelines <pipelines@acquia.com>
2020-04-14 17:53:45 +0000

    Pipelines job "'788cbf69-7e8e-4d44-807f-43e9c190b35b'": committed build result of git@github.com:Drupal4Gov/Drupal-GovCon-2017.git :develop.

---
+++ docroot/modules/custom/capitalcamp_blocks/src/Plugin/Block/DgcHero.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\capitalcamp_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides Hero Block.
+ *
+ * @Block(
+ *   id = "dgc_hero",
+ *   admin_label = @Translation("DGC Hero")
+ * )
+ */
+class DgcHero extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#type' => 'markup',
+      '#markup' => '',
+    ];
+  }
+
+}
---
+++ docroot/modules/custom/capitalcamp_glue/src/Plugin/views/field/Speakers.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\capitalcamp_glue\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Drupal\user\Entity\User;
+use Drupal\Core\Link;
+
+/**
+ * Field handler to flag the node type.
+ *
+ * @ViewsField("speakers")
+ */
+class Speakers extends FieldPluginBase {
+
+  /**
+   * Leave empty to avoid a query on this field.
+   */
+  public function query() {
+
+  }
+
+  /**
+   * Render function for the speakers field.
+   *
+   * Displays usernames for both node author and co-speakers field(s).
+   *
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $presenters = [];
+    $names = NULL;
+    $options = [
+      'absolute' => TRUE,
+      'attributes' => [
+        'class' => "views-field-uid",
+      ],
+    ];
+    /** @var \Drupal\node\Entity\Node $node */
+    $node = $values->_entity;
+
+    // Primary Speaker (is node Author).
+    $uid = $node->get("uid")->getString();
+    $user = User::load($uid);
+    $presenters[] = Link::createFromRoute($user->name->value, 'entity.user.canonical', ['user' => $uid], $options)->toString();
+
+    // Co-speakers.
+    if ($node->hasField("field_session_co_presenter_s_")) {
+      $co = $node->get('field_session_co_presenter_s_')->referencedEntities();
+      if ($co) {
+        foreach ($co as $speaker) {
+          $presenters[] = Link::createFromRoute($speaker->name->value, 'entity.user.canonical', ['user' => $speaker->uid->value], $options)->toString();
+        }
+      }
+    }
+
+    for ($i = 0; $i < count($presenters) - 1; $i++) {
+      $names .= $presenters[$i] . ", ";
+    }
+    $names .= $presenters[count($presenters) - 1];
+
+    return [
+      '#markup' => $names,
+    ];
+  }
+
+}
