7b4690e5b88e8dc2cfed06697d7b8d630152b723
pipelines <pipelines@acquia.com>
2022-02-28 19:37:48 +0000

    Pipelines job "'14900a6e-b027-4661-9163-8910c3e5b088'": committed build result of git@github.com:Drupal4Gov/Drupal-GovCon-2017.git :develop.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit867d9470c2864bb929d0f8d3d01f46cd::getLoader();
+return ComposerAutoloaderInit8c5106d6f9f35c8231e8249794a0610f::getLoader();
